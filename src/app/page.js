6ce84c83c168b9b63f59a0f3406c334ef98d21de
Fahrul Razi <fahrul.razix@gmail.com>
2023-11-27 22:40:03 +0700

    setup API, nextui, toast-react

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,19 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
-  return <div>EVENTMAKERS</div>;
+import { Button } from "@nextui-org/react";
+import toast from "react-hot-toast";
+
+const handleButton = () => {
+  toast.success("We Can Do It...!!!");
+};
+
+export default function Page() {
+  return (
+    <div className="justify-center px-2 m-3">
+      <div>EVENTMAKERS</div>
+      <Button onClick={handleButton} color="success">
+        Push Me
+      </Button>
+    </div>
+  );
 }
